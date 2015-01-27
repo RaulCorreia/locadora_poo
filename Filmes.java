8793a90fe063155ed823db0cf4fe547cc9b01542
Raul Correia <new_correia@yahoo.com.br>
2015-01-26 21:54:10 -0300

    Classe Filmes
    Classe Filmes

---
+++ Filmes.java
@@ -0,0 +1,46 @@
+
+public class Filmes {
+	
+	private String genero;
+	private String titulo;
+	private int ano;
+	
+	
+	public Filmes(String genero, String titulo, int ano){
+		
+		
+	}
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public int getAno() {
+		return ano;
+	}
+
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	
+
+}
